8df6de58667dc6313905afb8c6bc79d0156e1809
Ruben Croall <122880204+Rubbaducky95@users.noreply.github.com>
2024-02-15 21:46:22 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,17 @@
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
-        ReadOutput whatToRead = new ReadOutput("C:\\Users\\ruben\\OneDrive" +
-                "\\Dokument\\Högskolan\\DCTVT24" +
-                "\\EXAMENS ARBETE\\Simulation\\Simulations" +
-                "\\Test\\output" +
-                "\\busshallplats_hogskolan_2024-02-07_17-19-56.579");
+        String filePath = "C:\\Users\\ruben\\OneDrive" +
+                "\\Documents\\Datateknik VT 24\\EXAMENSARBETE\\Simulations\\Test\\output\\";
+        String simulationPath = "Straight_line_2024-02-15_17-29-41.945";
+        ReadOutput whatToRead = new ReadOutput(filePath + simulationPath);
 
-        System.out.println(whatToRead.meanAcceleration(1));
-        System.out.println(whatToRead.maxAcceleration(1));
-        System.out.println(whatToRead.accelerationAtGivenTime(1, 4));
+        ArrayList<Node> n = whatToRead.getXDataAt(30,3,1);
+        System.out.println(n);
+        System.out.println(whatToRead.accelerationAtGivenTime(1,30));
+        System.out.println(whatToRead.distanceBetween(n.get(0),whatToRead.getDataAt(31,1)));
     }
 }
