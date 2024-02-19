c634177ab5fa3bdfb6fa9a9527028ada730aa25d
Ruben Croall <122880204+Rubbaducky95@users.noreply.github.com>
2024-02-19 15:53:21 +0100

    Update Model.java
    Added function to predict new position and look for change in velocity or distance

--- Model.java
+++ Model.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Model {
 
-    public int checkForChange(ArrayList<Node> nList){
+    public static int checkForChange(ArrayList<Node> nList){
 
         if(nList.size() < 2) //Too few nodes to use in prediction
             return 0;
@@ -19,7 +19,29 @@ public class Model {
         else
             return 1; //No significant change, we keep going straight.
     }
-    public double[] predictNextPosition(ArrayList<Node> n) {
-        return null;
+    public static double[] predictNextPosition(ArrayList<Node> n) { //Input must be a list of all nodes up until the current position.
+
+        double[] newPos = new double[2];
+
+        if(n.size() == 1){
+            newPos[0] = n.get(0).x;
+            newPos[1] = n.get(0).y;
+            return newPos;
+        }
+
+        int change = checkForChange(n);
+        double t = 0.1; //Change this value depending on what you simulate with.
+        Node curPos = n.get(n.size() - 1);
+        Node prevPos = n.get(n.size() - 2);
+
+        if(change == 1) { //We are accelerating or have a constant velocity.
+            newPos[0] = curPos.x + prevPos.getVx(curPos) * t;
+            newPos[1] = curPos.y + prevPos.getVy(curPos) * t;
+        }
+        else if(change == -1) {
+            //Do something with the angle also hmm...
+        }
+
+        return newPos;
     }
 }
