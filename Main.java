2f7efeaa722d1425a540c972900af522863c29f4
Ruben Croall <122880204+Rubbaducky95@users.noreply.github.com>
2024-02-28 01:53:47 +0100

    Update Main.java
    Added in main to make .txt file of resultsm, should make into a method somewhere

--- Main.java
+++ Main.java
@@ -1,190 +1,71 @@
 import javax.swing.*;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-
-public class Main {
-    public static void main(String[] argimport javax.swing.*;
+import javax.swing.*;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+        public static void main(String[] args) throws FileNotFoundException {
 
-        String filePath = "C:\\Users\\ruben\\OneDrive\\Documents\\Datateknik VT 24\\EXAMENSARBETE\\Simulations\\Test\\output\\Zig_zag_2024-02-20_16-16-09.758";
-        ReadOutput whatToRead = new ReadOutput(filePath);
+            String filePath = "C:\\Users\\ruben\\OneDrive\\Dokument\\Högskolan\\DCTVT24\\EXAMENS ARBETE\\Simulation\\Simulations\\Test\\output\\Curve_2024-02-20_15-48-11.782";
+            ReadOutput whatToRead = new ReadOutput(filePath);
 
-        ArrayList<Node> VRU = whatToRead.getDataFor(1);
+            ArrayList<Double> RMSEforDifVRUs = new ArrayList<>();
+            StringBuilder output = new StringBuilder("pedestrianID no.dataPoints minRMSE\n");
+            double minRMSE;
+            int totNrVRUs = whatToRead.totalNrOfVRUs();
 
-        ArrayList<Node> oldpredictionVRU = Model.getPredictionList(VRU,0.5,2,0);
-        ArrayList<Node> newpredictionVRU = Model.getPredictionList(VRU,0.5,5,1);
+            for(int i = 1; i <= totNrVRUs; i++) {
 
-        for (int i = 1; i < VRU.size(); i++)
-            System.out.println(VRU.get(i) + " " + newpredictionVRU.get(i-1));
+                ArrayList<Node> VRU = whatToRead.getDataFor(i);
+                System.out.println("pedestrianID: " + i);
+                output.append(i).append(" ");
 
-        System.out.println(Model.positionalRMSE(VRU,oldpredictionVRU));
-        System.out.println(Model.positionalRMSE(VRU,newpredictionVRU));
+                for(int j = 1; j < 10; j++) {
+                    RMSEforDifVRUs.add(Model.positionalRMSE(VRU, Model.getPredictionList(VRU, 0.5, j, 1)));
+                }
 
-        ArrayList<ArrayList<Node>> listOfNodeLists = new ArrayList<>();
-        listOfNodeLists.add(VRU);
-        listOfNodeLists.add(newpredictionVRU);
-
-        SwingUtilities.invokeLater(() -> {
-            MainFrame frame = new MainFrame(listOfNodeLists, 20.0);
-            frame.setVisible(true);
-        });
-
-
-        /*System.out.println(Model.positionalRMSE(VRU, 0.5, 4));
-        double[] RMSEcord = Model.coordinateRMSE(VRU, 0.5, 4);
-        System.out.println("x: " + RMSEcord[0] + ", " + "y: " + RMSEcord[1]);
-        ArrayList<Node> test = new ArrayList<>();
-
-        for(Node n : VRU) {
-            test.add(n);
-            System.out.println(Model.checkForChange(test, 2));
-        }
-
-        /*
-        ArrayList<Node> VRU1 = whatToRead.getDataFor(1);
-        ArrayList<Node> test = new ArrayList<>();
-        ArrayList<Double> xValues = new ArrayList<>();
-        ArrayList<Double> yValues = new ArrayList<>();
-        for(Node n : VRU1) {
-            xValues.add(n.getX());
-            yValues.add(n.getY());
-        }
-
-        for(Node n : VRU1) {
-            test.add(n);
-            System.out.println(Model.checkForChange(test));
-        }
-
-        int [] timeStep = whatToRead.timeStepsFor(1);
-        int time = timeStep[0];
-        ArrayList<Double> newXValues = new ArrayList<>();
-        ArrayList<Double> newYValues = new ArrayList<>();
-        double[] newpos;
-        for(Node n : VRU1) {
-            test.add(n);
-            newpos = Model.predictNextPosition(test);
-            time++;
-            System.out.println("Time step : " + time );
-            System.out.print("Predicted xy: ");
-            for(int i = 0; i < 2; i++) {
-                System.out.print(newpos[i] + " ");
+                minRMSE = Collections.min(RMSEforDifVRUs);
+                System.out.println("No. data points: " + (1+RMSEforDifVRUs.indexOf(minRMSE)));
+                output.append((1+RMSEforDifVRUs.indexOf(minRMSE))).append(" ");
+                System.out.println("Min RMSE: " + minRMSE);
+                output.append(Collections.min(RMSEforDifVRUs)).append("\n");
+                RMSEforDifVRUs.clear();
+                VRU.clear();
             }
-            System.out.print("\nActual position: " + xValues.get(time-1) + " " + yValues.get(time-1));
-            System.out.println("\n");
-            newXValues.add(newpos[0]);
-            newYValues.add(newpos[1]);
-        }
 
-        double RMSEx = 0.0;
-        double RMSEy = 0.0;
-        for(int i = 0; i < timeStep[1]-timeStep[0]+1; i++){
-            RMSEx += Math.pow(xValues.get(i+1) - newXValues.get(i),2);
-            RMSEy += Math.pow(yValues.get(i+1) - newYValues.get(i),2);
-        }
-        RMSEx = Math.sqrt(RMSEx / (timeStep[1] - timeStep[0]+1));
-        RMSEy = Math.sqrt(RMSEy / (timeStep[1] - timeStep[0]+1));
-
-
-        System.out.println(timeStep[1]-timeStep[0]);
-        System.out.println("Old x values: " + xValues.size() + ", New x values: " + newXValues.size());
-        System.out.println(RMSEx + ", " + RMSEy);
-         */
-
-    }
-}s) throws FileNotFoundException {
-
-        String filePath = "C:\\Users\\ruben\\OneDrive\\Dokument\\Högskolan\\DCTVT24\\EXAMENS ARBETE\\Simulation\\Simulations\\Test\\output\\Curve_2024-02-20_15-48-11.782";
-        ReadOutput whatToRead = new ReadOutput(filePath);
-
-        ArrayList<Node> VRU = whatToRead.getDataFor(1);
-        ArrayList<Node> predictedVRU = Model.getPredictionList(VRU);
-
-        ArrayList<Node> test = new ArrayList<>();
-
-        for(int i = 0; i < 10; i++)
-            test.add(VRU.get(i));
-
-        double[] testPoly = Model.predictionPolynomialRegression(test, 3, 0.5);
-
-        System.out.println(test);
-        System.out.println("\n" + "Predicted position, timestep 11: \n" + "x: " + testPoly[0] + " y: " + testPoly[1]);
-
-
-
-        /*
-        ArrayList<ArrayList<Node>> listOfNodeLists = new ArrayList<>();
-        listOfNodeLists.add(VRU);
-        listOfNodeLists.add(predictedVRU);
-
-        SwingUtilities.invokeLater(() -> {
-            MainFrame frame = new MainFrame(listOfNodeLists, 20.0);
-            frame.setVisible(true);
-        });
-        */
-
-        /*System.out.println(Model.positionalRMSE(VRU, 0.5, 4));
-        double[] RMSEcord = Model.coordinateRMSE(VRU, 0.5, 4);
-        System.out.println("x: " + RMSEcord[0] + ", " + "y: " + RMSEcord[1]);
-        ArrayList<Node> test = new ArrayList<>();
-
-        for(Node n : VRU) {
-            test.add(n);
-            System.out.println(Model.checkForChange(test, 2));
-        }
-
-        /*
-        ArrayList<Node> VRU1 = whatToRead.getDataFor(1);
-        ArrayList<Node> test = new ArrayList<>();
-        ArrayList<Double> xValues = new ArrayList<>();
-        ArrayList<Double> yValues = new ArrayList<>();
-        for(Node n : VRU1) {
-            xValues.add(n.getX());
-            yValues.add(n.getY());
-        }
-
-        for(Node n : VRU1) {
-            test.add(n);
-            System.out.println(Model.checkForChange(test));
-        }
-
-        int [] timeStep = whatToRead.timeStepsFor(1);
-        int time = timeStep[0];
-        ArrayList<Double> newXValues = new ArrayList<>();
-        ArrayList<Double> newYValues = new ArrayList<>();
-        double[] newpos;
-        for(Node n : VRU1) {
-            test.add(n);
-            newpos = Model.predictNextPosition(test);
-            time++;
-            System.out.println("Time step : " + time );
-            System.out.print("Predicted xy: ");
-            for(int i = 0; i < 2; i++) {
-                System.out.print(newpos[i] + " ");
+            try {
+                FileWriter writer = new FileWriter("result.txt");
+                writer.write(String.valueOf(output));
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            System.out.print("\nActual position: " + xValues.get(time-1) + " " + yValues.get(time-1));
-            System.out.println("\n");
-            newXValues.add(newpos[0]);
-            newYValues.add(newpos[1]);
-        }
-
-        double RMSEx = 0.0;
-        double RMSEy = 0.0;
-        for(int i = 0; i < timeStep[1]-timeStep[0]+1; i++){
-            RMSEx += Math.pow(xValues.get(i+1) - newXValues.get(i),2);
-            RMSEy += Math.pow(yValues.get(i+1) - newYValues.get(i),2);
-        }
-        RMSEx = Math.sqrt(RMSEx / (timeStep[1] - timeStep[0]+1));
-        RMSEy = Math.sqrt(RMSEy / (timeStep[1] - timeStep[0]+1));
 
 
-        System.out.println(timeStep[1]-timeStep[0]);
-        System.out.println("Old x values: " + xValues.size() + ", New x values: " + newXValues.size());
-        System.out.println(RMSEx + ", " + RMSEy);
-         */
+/*
+            for (int i = 1; i < VRU.size(); i++)
+                System.out.println(VRU.get(i) + " " + newpredictionVRU.get(i-1));
+
+            System.out.println(Model.positionalRMSE(VRU,oldpredictionVRU));
+            System.out.println(Model.positionalRMSE(VRU,newpredictionVRU));
+
+
+ */
+/*
+            ArrayList<ArrayList<Node>> listOfNodeLists = new ArrayList<>();
+            listOfNodeLists.add(whatToRead.getDataFor(10));
+            listOfNodeLists.add(Model.getPredictionList(listOfNodeLists.get(0),0.5,3,1));
+
+            SwingUtilities.invokeLater(() -> {
+                MainFrame frame = new MainFrame(listOfNodeLists, 20.0);
+                frame.setVisible(true);
+            });
+            */
 
     }
 }
