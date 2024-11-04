8d2dabdb99ce97945ba9a3c33158a0e613b2802a
Ruben Croall <122880204+Rubbaducky95@users.noreply.github.com>
2024-11-04 22:45:25 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,70 +11,53 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
-        String filePath = "C:\\Users\\ruben\\OneDrive\\Dokument\\Högskolan\\DCTVT24\\EXAMENS ARBETE\\Simulation\\Simulations\\Test\\output\\";
+        String filePath = "C:\\Users\\ruben\\OneDrive\\Documents\\Datateknik VT 24\\EXAMENSARBETE\\Simulations\\Test\\output\\";
         String fileName = "Z-turn_0.9s_100VRUs";
         ReadOutput whatToRead = new ReadOutput(filePath + fileName);
+        ToTxt.APFPForVRU(whatToRead,0.9,2,1,1);
 
-        ToTxt.actualPositionsForVRU(whatToRead,3);
-        ToTxt.APFPForVRU(whatToRead,0.9,3,3);
+        /*double meanAccuracy = 0;
+        int n = whatToRead.totalNrOfVRUs();
 
-        //ToTxt.predictedPositionsForVRU(whatToRead,0.9,2,1,0);
-        //ToTxt.predictedPositionsForVRU(whatToRead,0.9,3,1,0);
-        //ToTxt.predictedPositionsForVRU(whatToRead,0.9,0,1,1);
+        for (int i = 1; i < n; i++) {
+            ArrayList<APFP> apfp = Model.getAPFPList(whatToRead.getDataFor(i), 0.9, 3, 2);
+            meanAccuracy += Model.getAccuracy(apfp);
+        }
+        System.out.println(meanAccuracy / n);*/
+
+        //ToTxt.actualPositionsForVRU(whatToRead,2);
+        //ToTxt.APFPForVRU(whatToRead,0.9,3,1,2);
+
+        /*ArrayList<Node> actualPos = whatToRead.getDataFor(5);
+        ArrayList<APFP> areas = Model.getAPFPList(actualPos, 0.9, 3, 0);
+
+        System.out.println(Model.getAccuracy(areas));*/
+
+        /*ToTxt.predictedPositionsForVRU(whatToRead,0.9,3,2,1);
+        ToTxt.predictedMeanPositionsForVRU(whatToRead,0.9,4,2);
+        ToTxt.predictedMeanPositionsForVRU(whatToRead,0.9,3,2);*/
 
         /*ToTxt.minMaxRMSEforVRUsWithSpecDataPoints(whatToRead,0.9,2,0);
         ToTxt.minMaxRMSEforVRUsWithSpecDataPoints(whatToRead,0.9,3,0);
-        ToTxt.minMaxRMSEforVRUsWithSpecDataPoints(whatToRead,0.9,0,1);
-        ToTxt.minMaxRMSEforVRUsWithMeanPrediction(whatToRead,0.9);*/
+        ToTxt.minMaxRMSEforVRUsWithSpecDataPoints(whatToRead,0.9,4,0);
+        ToTxt.minMaxRMSEforVRUsWithSpecDataPoints(whatToRead,0.9,5,0);*/
 
         /*ToTxt.predictedMeanPositionsForVRU(whatToRead,0.9,2,4);
         ToTxt.predictedMeanPositionsForVRU(whatToRead,0.9,3,4);
         ToTxt.predictedMeanPositionsForVRU(whatToRead,0.9,4,4);
         ToTxt.predictedMeanPositionsForVRU(whatToRead,0.9,5,4);*/
 
-        /*ToTxt.minMaxRMSEforVRUsWithMeanPrediction(whatToRead, 0.9, 2);
+        /*ToTxt.minMaxRMSEforVRUsWithSpecDataPoints(whatToRead,0.9,3,1);
         ToTxt.minMaxRMSEforVRUsWithMeanPrediction(whatToRead, 0.9, 3);
-        ToTxt.minMaxRMSEforVRUsWithMeanPrediction(whatToRead, 0.9, 4);
-        ToTxt.minMaxRMSEforVRUsWithMeanPrediction(whatToRead, 0.9, 5);*/
+        ToTxt.minMaxRMSEforVRUsWithMeanPrediction(whatToRead, 0.9, 4);*/
 
 //      ToTxt.RMSEforVRUsWithSpecDataPoints(whatToRead,0.9,2,0);
 //      ToTxt.RMSEforVRUsWithSpecDataPoints(whatToRead,0.9,3,0);
 //      ToTxt.RMSEforVRUsWithSpecDataPoints(whatToRead,0.9,0,1);
+        //ToTxt.differenceListForAllVRUs(whatToRead,0.9,3,2);
+        //ToTxt.differenceListForAllVRUs(whatToRead,0.9,4,2);
         //ToTxt.differenceListForAllVRUs(whatToRead,0.9,3,1);
-        //ToTxt.minMaxRMSEforDataPoints(whatToRead,0.9,2,6,1);
-
-        /*ArrayList<Node> VRU = whatToRead.getDataFor(2);
-
-        Node n1 = VRU.get(0);
-        Node n2 = VRU.get(1);
-
-        ArrayList<Double> angles = new ArrayList<>();
-
-        for (int i = 2; i < VRU.size(); i++) {
-            angles.add(VRU.get(i).angleBetween(n1,n2));
-            n1 = VRU.get(i-1);
-            n2 = VRU.get(i);
-        }
-
-        ArrayList<Node> test = new ArrayList<>();
-        ArrayList<Integer> changeFlags = new ArrayList<>();
-
-        for(Node n : VRU) {
-            test.add(n);
-            changeFlags.add(Model.checkForChange(test));
-        }
-        System.out.println(angles);
-        System.out.println(changeFlags);*/
-
-//        ArrayList<ArrayList<Node>> listOfNodeLists = new ArrayList<>();
-//        listOfNodeLists.add(whatToRead.getDataFor(2));
-//        listOfNodeLists.add(Model.getPredictionList(listOfNodeLists.get(0),0.9,0,1));
-//
-//        SwingUtilities.invokeLater(() -> {
-//            MainFrame frame = new MainFrame(listOfNodeLists, 20.0);
-//            frame.setVisible(true);
-//        });
-
+        //ToTxt.minMaxRMSEforDataPoints(whatToRead,0.9,2,6,0);
 
     }
 }
